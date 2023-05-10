4e41d7b8a59724ecc8c590eb6d327c76386a98e9
Aleksey <easy.mail.to.copy@gmail.com>
2023-05-10 22:54:56 +0300

    Solved hw06

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -19,7 +19,6 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
 ) => {
     const finalClassName = s.button + ' ' + (disabled ? s.disabled : xType === 'red' ? s.red : xType==='secondary'? s.secondary : s.default)
 
-
     return (
         <button
             disabled={disabled}
