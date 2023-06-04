e7dcf40a1807589d7259a0ce776722159b223689
Aleksey <easy.mail.to.copy@gmail.com>
2023-06-04 20:48:38 +0300

    Solved hw10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -13,14 +13,19 @@ import {Loader} from './Loader'
 * 4 - сделать стили в соответствии с дизайном
 * */
 
+
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = useSelector<AppStoreType, { isLoading: boolean } >(state=> state.loading)
+
+    const dispatch = useDispatch()
+
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-
+        dispatch(loadingAC(true))
         // setTimeout
+        setTimeout(()=>dispatch(loadingAC(false)),1500 )
     }
 
     return (
@@ -28,7 +33,7 @@ const HW10 = () => {
             <div className={s2.hwTitle}>Homework #10</div>
 
             <div className={s2.hw}>
-                {isLoading ? (
+                {isLoading.isLoading ? (
                     <div id={'hw10-loading'}>
                         <Loader/>
                     </div>
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,14 +2,18 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType):  { isLoading: boolean }  => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case "CHANGE_LOADING":
+            debugger
+            return {...state, isLoading: action.isLoading}
 
         default:
             return state
     }
 }
+
 
 type LoadingActionType = {
     type: 'CHANGE_LOADING'
